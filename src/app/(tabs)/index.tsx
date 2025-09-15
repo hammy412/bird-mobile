8eebb7e4584ff44d7d4777480250920d08081e37
Aidan <132329147+hammy412@users.noreply.github.com>
2025-09-14 20:05:26 -0400

    9/14

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,15 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, Image } from 'react-native';
+import { Audio } from 'expo-av';
+import { useState, useEffect } from 'react';
+import { supabase } from '../../lib/supabase';
 
 export default function App() {
   return (
     <View className='flex-1 p-4'>
       <Text className='text-3xl font-bold text-red-500'>Hello World</Text>
-      <Link href='/login'>Login</Link>
+      <Link href='/login' className='text-white'>Login</Link>
     </View>
   );
 }
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
+import { AppState, Platform } from 'react-native'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
@@ -16,3 +17,13 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
         detectSessionInUrl: false,
     },
 });
+
+if (Platform.OS !== "web") {
+    AppState.addEventListener('change', (state) => {
+      if (state === 'active') {
+        supabase.auth.startAutoRefresh()
+      } else {
+        supabase.auth.stopAutoRefresh()
+      }
+    })
+  }
